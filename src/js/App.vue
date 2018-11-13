b95a9873d0431783c439b2a938b33c53e67b66e8
shohei.koda <shohei.koda@gooya.co.jp>
2018-11-13 19:55:06 +0900

    sample source

---
+++ src/js/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="music">
+        <at-button @click="showMessage">Show Message</at-button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    methods: {
+        pushKey: function() {
+
+        }
+    }
+}
+
+new Vue({
+    props: ['key'],
+    methods: {
+        showMessage: function() {
+
+        var synth = new Tone.Synth().toMastere();
+        synth.triggerAttackRelease('C5', '4n');
+            this.$Message('Thanks for using AT-UI')
+        }
+    }
+}).$mount('#app')
+
+</script>
+
